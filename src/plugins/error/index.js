7e49d2f722dbe47e2a6ea42e804a313859c2ef44
chenxi <bbxycx.18@163.com>
2020-01-03 20:30:59 +0800

    init

--- src/plugins/error/index.js
+++ src/plugins/error/index.js
@@ -5,16 +5,13 @@ export default {
     install (Vue, options) {
         Vue.config.errorHandler = function (error, instance, info) {
             Vue.nextTick(() => {
-                // store 追加 log
                 store.dispatch('admin/log/push', {
                     message: `${info}: ${error.message}`,
                     type: 'error',
                     meta: {
                         error
-                        // instance
                     }
                 });
-                // 只在开发模式下打印 log
                 if (process.env.NODE_ENV === 'development') {
                     util.log.capsule('MQCMS Admin', 'ErrorHandler', 'error');
                     util.log.error('>>>>>> 错误信息 >>>>>>');
