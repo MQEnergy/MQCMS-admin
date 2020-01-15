2337161a8826773b7880e0b20839a7c218a2ca76
chenxi <bbxycx18@gmail.com>
2020-01-15 14:40:03 +0800

    dashboard

--- src/pages/dashboard/console/index.vue
+++ src/pages/dashboard/console/index.vue
@@ -1,6 +1,17 @@
 <template>
-    <div style="padding-top: 16px;" v-resize="handleResize">
-        MQCMS Admin Dashboard
+    <div style="padding-top: 46px; text-align: center" v-resize="handleResize">
+        <img src="@/assets/images/logo.png" alt="">
+        <p>
+            MQCMS是一款现代化，快速，高效，灵活，前后端分离，扩展性强的CMS系统。
+            MQCMS中的MQ取麻雀拼音首字母。寓意麻雀虽小五脏俱全。
+        </p>
+        <p style="margin-top: 20px;">
+            前端项目仓库：
+            <i-link link-color="true" target="_blank" href="https://github.com/MQEnergy/MQCMS-admin">https://github.com/MQEnergy/MQCMS-admin</i-link>
+            <br>
+            后端项目仓库：
+            <i-link link-color="true" target="_blank" href="https://github.com/MQEnergy/MQCMS">https://github.com/MQEnergy/MQCMS</i-link>
+        </p>
     </div>
 </template>
 <script>
@@ -9,10 +20,11 @@
     import visitChart from './visit-chart';
     import hotSearch from './hot-search';
     import userPreference from './user-preference';
+    import ILink from '@/components/link';
 
     export default {
         name: 'dashboard-console',
-        components: { baseInfo, gridMenu, visitChart, hotSearch, userPreference },
+        components: { baseInfo, gridMenu, visitChart, hotSearch, userPreference, ILink },
         data () {
             return {
                 visitType: 'day', // day, month, year
