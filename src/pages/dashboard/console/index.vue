99dfba8bf102e7dad82dcac272378d3102d0c288
chenxi <bbxycx18@gmail.com>
2020-01-15 14:55:58 +0800

    dashboard

--- src/pages/dashboard/console/index.vue
+++ src/pages/dashboard/console/index.vue
@@ -2,15 +2,25 @@
     <div style="padding-top: 46px; text-align: center" v-resize="handleResize">
         <img src="@/assets/images/logo.png" alt="">
         <p>
-            MQCMS是一款现代化，快速，高效，灵活，前后端分离，扩展性强的CMS系统。
-            MQCMS中的MQ取麻雀拼音首字母。寓意麻雀虽小五脏俱全。
+            <span style="font-size: 18px">
+                基于hyperf的快速优雅的应用开发框架
+            </span>
+            <br>
+            <br>
+            <span style="font-size: 16px; color: #999">
+                MQCMS是一款现代化，快速，高效，灵活，前后端分离，扩展性强的CMS系统。
+                MQCMS中的MQ取麻雀拼音首字母。寓意麻雀虽小五脏俱全。
+            </span>
         </p>
-        <p style="margin-top: 20px;">
+        <p style="width: 450px; margin: 0 auto; margin-top: 30px; text-align: left">
+            后端项目仓库：
+            <i-link link-color="true" target="_blank" href="https://github.com/MQEnergy/MQCMS">https://github.com/MQEnergy/MQCMS</i-link>
+            <br>
             前端项目仓库：
             <i-link link-color="true" target="_blank" href="https://github.com/MQEnergy/MQCMS-admin">https://github.com/MQEnergy/MQCMS-admin</i-link>
             <br>
-            后端项目仓库：
-            <i-link link-color="true" target="_blank" href="https://github.com/MQEnergy/MQCMS">https://github.com/MQEnergy/MQCMS</i-link>
+            小程序项目仓库：
+            <i-link link-color="true" target="_blank" href="https://github.com/MQEnergy/MQCMS-miniprogram">https://github.com/MQEnergy/MQCMS-miniprogram</i-link>
         </p>
     </div>
 </template>
